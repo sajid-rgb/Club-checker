71faf5da4d018360a836c32cfc90aed68cd75f85
Sajid Chowdhury <msrchk@gmail.com>
2021-03-11 14:05:54 +0600

    last push

--- src/Components/TeamDetailsCard/TeamDetailsCard.js
+++ src/Components/TeamDetailsCard/TeamDetailsCard.js
@@ -8,14 +8,16 @@ import TeamDetailsHeader from '../../TeamDetailsHeader/TeamDetailsHeader';
 import { faFutbol } from '@fortawesome/free-regular-svg-icons';
 const TeamDetailsCard = (props) => {
     const { team } = props
-    const { strTeam, intFormedYear, strSport, strStadiumThumb, strTeamBadge, strDescriptionEN, strDescriptionDE, strWebsite, strFacebook, strTwitter, strInstagram, strCountry, strGender, strYoutube } = team;
+    const { strTeam, intFormedYear, strSport, strStadiumThumb, strTeamBadge, strDescriptionEN, strDescriptionDE, strWebsite, strFacebook, strTwitter, strInstagram, strCountry, strGender, strYoutube,strTeamShort } = team;
     return (
         <div >
             <TeamDetailsHeader stadium={strStadiumThumb} logo={strTeamBadge}></TeamDetailsHeader>
             <div className='row  mt-3 container mx-auto  ' style={{ backgroundColor: 'revert' }}>
-                <Card className="col-md-7 bg-danger border-0" >
+                <Card className="col-md-7 bg-success border-0" >
                     <Card.Body className='text-white'>
-                        <Card.Title><h2>{strTeam}</h2></Card.Title>
+                        <Card.Title><h2>{strTeam} {
+                            strTeamShort!==null && <span>({strTeamShort})</span>
+                        }</h2></Card.Title>
                         <Card.Text className='' style={{ fontSize: '18px', fontWeight: 'bold' }}><p ><FontAwesomeIcon icon={faMapMarkerAlt} /><span className='ml-2'>Founded: {intFormedYear}</span></p>
                             <p className='pr-5'> <FontAwesomeIcon icon={faFlag} /><span className='ml-1'>Country: {strCountry} </span> </p>
                             <p><FontAwesomeIcon icon={faFutbol} /><span className='ml-1'>Sport Type: {strSport}</span></p>
@@ -24,7 +26,7 @@ const TeamDetailsCard = (props) => {
                     </Card.Body>
 
                 </Card>
-                <Card className="col-md-5 bg-danger border-0  ">
+                <Card className="col-md-5 bg-success border-0  ">
                     {
                         strGender === "Male" ? <Card.Img src={male} className=' w-75 ml-5  mt-4 mb-2 mb-md-0'></Card.Img>
                             : <Card.Img src={female} className=' w-75 ml-5  mt-4 mb-2 mb-md-0'></Card.Img>
@@ -32,7 +34,7 @@ const TeamDetailsCard = (props) => {
                 </Card>
 
             </div>
-            <div className="text-white container text-justify text-white">
+            <div className="text-white container text-justify text-white container mt-3 p-3" >
                 <p>{strDescriptionEN}</p>
                 <p>{strDescriptionDE}</p>
             </div>
