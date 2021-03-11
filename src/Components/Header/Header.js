0c366c0a22f024485cac63d2d5db44d3ca390d74
Sajid Chowdhury <msrchk@gmail.com>
2021-03-11 15:46:44 +0600

    last push

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,8 +5,8 @@ import './Header.css'
 const Header = () => {
     return (
         <div className='header d-flex align-self-end justify-content-center flex-column pt-md-5 pt-0 '>
-            <a href="/home" style={{textDecoration:'none'}}><h1 className='pt-5'>Team Collection</h1>
-            <p className="text-center pb-3 text-white" style={{ fontWeight: 'bold', fontSize: '20px' }}><small>Check your favorite team any time</small></p></a>
+            <a href="/home" style={{textDecoration:'none'}}><h1 className='pt-5'>CLUB CHECKER</h1>
+            <p className="text-center pb-3 text-white" style={{ fontWeight: 'bold', fontSize: '20px' }}><small>Check your favorite club any time</small></p></a>
         </div>
     );
 };
